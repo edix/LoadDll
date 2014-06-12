6c9bdeaa6d89096eccc8038b6ba5704789be2148
edix <edix@users.noreply.github.com>
2014-06-12 23:18:42 +0200

    removed unused code in LoadDll.cpp

--- LoadDll/LoadDll.cpp
+++ LoadDll/LoadDll.cpp
@@ -1,5 +1,5 @@
 
-// LoadDll.cpp: Definiert das Klassenverhalten für die Anwendung.
+// LoadDll.cpp: Definiert das Klassenverhalten fï¿½r die Anwendung.
 //
 
 #include "stdafx.h"
@@ -22,7 +22,7 @@ END_MESSAGE_MAP()
 
 CLoadDllApp::CLoadDllApp()
 {
-	// TODO: Hier Code zur Konstruktion einfügen
+	// TODO: Hier Code zur Konstruktion einfï¿½gen
 	// Alle wichtigen Initialisierungen in InitInstance positionieren
 }
 
@@ -36,13 +36,13 @@ CLoadDllApp theApp;
 
 BOOL CLoadDllApp::InitInstance()
 {
-	// InitCommonControlsEx() ist für Windows XP erforderlich, wenn ein Anwendungsmanifest
-	// die Verwendung von ComCtl32.dll Version 6 oder höher zum Aktivieren
+	// InitCommonControlsEx() ist fï¿½r Windows XP erforderlich, wenn ein Anwendungsmanifest
+	// die Verwendung von ComCtl32.dll Version 6 oder hï¿½her zum Aktivieren
 	// von visuellen Stilen angibt. Ansonsten treten beim Erstellen von Fenstern Fehler auf.
 	INITCOMMONCONTROLSEX InitCtrls;
 	InitCtrls.dwSize = sizeof(InitCtrls);
 	// Legen Sie dies fest, um alle allgemeinen Steuerelementklassen einzubeziehen,
-	// die Sie in Ihrer Anwendung verwenden möchten.
+	// die Sie in Ihrer Anwendung verwenden mï¿½chten.
 	InitCtrls.dwICC = ICC_WIN95_CLASSES;
 	InitCommonControlsEx(&InitCtrls);
 
@@ -50,39 +50,30 @@ BOOL CLoadDllApp::InitInstance()
 
 
 	// Shell-Manager erstellen, falls das Dialogfeld
-	// Shellstrukturansicht- oder Shelllistenansicht-Steuerelemente enthält.
+	// Shellstrukturansicht- oder Shelllistenansicht-Steuerelemente enthï¿½lt.
 	CShellManager *pShellManager = new CShellManager;
-
-	// Standardinitialisierung
-	// Wenn Sie diese Features nicht verwenden und die Größe
-	// der ausführbaren Datei verringern möchten, entfernen Sie
-	// die nicht erforderlichen Initialisierungsroutinen.
-	// Ändern Sie den Registrierungsschlüssel, unter dem Ihre Einstellungen gespeichert sind.
-	// TODO: Ändern Sie diese Zeichenfolge entsprechend,
-	// z.B. zum Namen Ihrer Firma oder Organisation.
-	SetRegistryKey(_T("Vom lokalen Anwendungs-Assistenten generierte Anwendungen"));
 
 	CLoadDllDlg dlg;
 	m_pMainWnd = &dlg;
 	INT_PTR nResponse = dlg.DoModal();
 	if (nResponse == IDOK)
 	{
-		// TODO: Fügen Sie hier Code ein, um das Schließen des
-		//  Dialogfelds über "OK" zu steuern
+		// TODO: Fï¿½gen Sie hier Code ein, um das Schlieï¿½en des
+		//  Dialogfelds ï¿½ber "OK" zu steuern
 	}
 	else if (nResponse == IDCANCEL)
 	{
-		// TODO: Fügen Sie hier Code ein, um das Schließen des
-		//  Dialogfelds über "Abbrechen" zu steuern
+		// TODO: Fï¿½gen Sie hier Code ein, um das Schlieï¿½en des
+		//  Dialogfelds ï¿½ber "Abbrechen" zu steuern
 	}
 
-	// Den oben erstellten Shell-Manager löschen.
+	// Den oben erstellten Shell-Manager lï¿½schen.
 	if (pShellManager != NULL)
 	{
 		delete pShellManager;
 	}
 
-	// Da das Dialogfeld geschlossen wurde, FALSE zurückliefern, sodass wir die
+	// Da das Dialogfeld geschlossen wurde, FALSE zurï¿½ckliefern, sodass wir die
 	//  Anwendung verlassen, anstatt das Nachrichtensystem der Anwendung zu starten.
 	return FALSE;
 }
